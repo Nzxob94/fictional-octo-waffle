d902e43fd1cbafc26db14b803e9392c63900c0c5
Efebey2903 <swatibaljeet0101@gmail.com>
2024-08-21 18:13:29 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -1,5 +1,5 @@
 net user runneradmin P@ssw0rd!
 choco install ngrok -y
 ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
-powershell -command Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -WindowStyle Hidden
+start /b ngrok tcp 3389
 sleep 240
