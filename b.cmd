352d9520384cd5dff3194021e7b34231e0b42724
Nzxob94 <Nzxob94@gmail.com>
2024-09-22 18:11:04 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -1,5 +1,5 @@
 net user runneradmin P@ssw0rd!
 choco install ngrok -y
-ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
+ngrok config add-authtoken 2mQnoSrnpFWZX5IHJSzdAzzBTpi_4NWtZDHkG7ZDCb2Lv3BFX
 start /b ngrok tcp 3389
 sleep 240
