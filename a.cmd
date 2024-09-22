dd689663c9436a2e4da0d8b92ca26f56039e3726
Nzxob94 <Nzxob94@gmail.com>
2024-09-22 17:53:51 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,5 +1,5 @@
 net user runneradmin P@ssw0rd!
 curl -s -L -o chromeremotedesktophost.msi https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
-curl -s -L -o tcp.ps1 https://github.com/Efebey2903/fictional-octo-waffle/raw/main/tcp.ps1
+curl -s -L -o tcp.ps1 https://github.com/Nzxob94/fictional-octo-waffle/blob/main/tcp.ps1
 msiexec /i D:\a\fictional-octo-waffle\fictional-octo-waffle\chromeremotedesktophost.msi /qn /norestart
 powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
